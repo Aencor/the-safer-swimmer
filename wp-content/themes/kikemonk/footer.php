5e37ab832c1a5af8b612096f7eb24f7e981e9bb1
LizzieSoriano <lizziesorianop@gmail.com>
2025-03-24 16:17:24 -0600

    footer

--- wp-content/themes/kikemonk/footer.php
+++ wp-content/themes/kikemonk/footer.php
@@ -1,37 +1,39 @@
-      <footer>
-        <div class="top-footer flex justify-between">
+    <footer>
+        <div class="top-footer">
+          <div class="max-w-screen-lg mx-auto flex justify-between">
             <div class="logo-container">
                 <img src="<?= bloginfo('template_url')?>/assets/img/logos/safer2.png"  alt="">
-              <div class="text-container">
-                <p>The Safer Swimmer is the parent company of Premier, overseeing a dedicated management team that provides expertise in operations, HR, marketing, finance, and executive leadership. Committed to excellence in aquatic safety and education, The Safer Swimmer supports its subsidiaries in delivering top-tier swim instruction, lifeguarding, and water safety training.</p>
-              </div> 
+                <div class="textfooter-container flex">
+                  <p>The Safer Swimmer is the parent company of Premier, overseeing a dedicated management team that provides expertise in operations, HR, marketing, finance, and executive leadership. Committed to excellence in aquatic safety and education, The Safer Swimmer supports its subsidiaries in delivering top-tier swim instruction, lifeguarding, and water safety training.</p>
+                </div> 
             </div>
-          <div class="list-container">
-              <div class="safer-container">
-                <h2> The Safer Swimmer</h2>
-              </div>
             <div class="list-container">
-                <ol>
-                  <li>
-                    6 Journey,Suite 200, Aliso Viejo, CA 92656
-                  </li>
-                  <li>
-                    (949) 716 - 3333 
-                  </li>
-                  <li>
-                    info@thesaferswimmer.com
-                  </li>
-                </ol>
+                <div class="safer-container">
+                  <h2> The Safer Swimmer</h2>
+                </div>
+              <div class="list-container flex">
+                  <ol>
+                    <li>
+                    <i class="fa-solid fa-location-dot"></i> 6 Journey,Suite 200, Aliso Viejo, CA 92656
+                    </li>
+                    <li>
+                    <i class="fa-solid fa-phone"></i>(949) 716 - 3333 
+                    </li>
+                    <li>
+                    <i class="fa-solid fa-envelope"></i>info@thesaferswimmer.com
+                    </li>
+                  </ol>
             </div>
+          </div>
         </div>
       </div>
       </div>
       <div class="bottom-footer">
-        <div class="copy-container flex items-center">
+        <div class="copy-container">
           <p>Copyright © 2025 The Safer Swimmer. All rights reserved.</p>
         </div>
       </div>
-      </footer>
+    </footer>
     <?php wp_footer(); ?>
   </body>
 </html>
